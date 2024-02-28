18c404ab4608a95eb9db3dc83e13f099074156be
Raisson Souza <raissonrai@gmail.com>
2024-02-27 23:06:14 -0300

    - Ajuste em AuthEndpoints; - Implementada resposta padrão em Endpoints.

--- src/services/AuthEndpoints.ts
+++ src/services/AuthEndpoints.ts
@@ -1,16 +1,24 @@
 import Env from "../config/Env"
 import Response from "../data/classes/Response"
+import Endpoints from "./base/Endpoints"
 
-export default abstract class AuthEndpoints
+export default abstract class AuthEndpoints extends Endpoints
 {
     static async ValidateJwt(jwt : string) {
-        const url = `${ Env.BaseBack }/validate_jwt?jwt=${ jwt }`
-
-        const response = await fetch(url, { mode: "cors", method: "GET" })
-            .then(async (res) => {
-                return new Response(await res.json())
-            })
-
-        return response
+        try
+        {
+            const url = `${ Env.BaseBack }/validate_jwt?jwt=${ jwt }`
+    
+            const response = await fetch(url, { mode: "cors", method: "GET" })
+                .then(async (res) => {
+                    return new Response(await res.json())
+                })
+    
+            return response
+        }
+        catch
+        {
+            return this.FailFetchResponse
+        }
     }
 }
---
+++ src/services/base/Endpoints.ts
@@ -0,0 +1,13 @@
+import Response from "../../data/classes/Response"
+
+export default abstract class Endpoints
+{
+    static FailFetchResponse : Response = new Response(
+        {
+            "success": false,
+            "data": null,
+            "length": 0,
+            "action": ""
+        }
+    )
+}
