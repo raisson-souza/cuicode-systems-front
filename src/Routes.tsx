5fd0f0e2b35400b6c038a024e6e02bc6bb59875b
Raisson Souza <raissonrai@gmail.com>
2024-02-28 00:06:29 -0300

    - Refatoração de código.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,7 @@
 // https://www.youtube.com/watch?v=oTIJunBa6MA&t=107s
 // https://reactrouter.com/en/main/routers/create-browser-router
 
-import {
-    createBrowserRouter
-} from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
 import ExternalHome from "./screens/Home/ExternalHome"
 import DefaultError from "./screens/Error/DefaultError"
 import User from "./screens/Users/User"
@@ -14,37 +12,83 @@ import LoginScreen from "./screens/Auth/Login"
 import AccountRecoveryScreen from "./screens/Auth/AccountRecovery"
 import ProtectedRoute from "./components/ProtectedRoute"
 
-function CreateRoute
-(
+type CreateRouteProp = {
     path : string,
     element : JSX.Element,
-    protectedRoute : boolean = true,
-    errorElement : JSX.Element = <DefaultError />
-) {
+    protectedRoute? : boolean
+}
+
+function CreateRoute({ path, element, protectedRoute = true } : CreateRouteProp) {
     return {
         "path": path,
         "element": protectedRoute
             ? (<ProtectedRoute>{ element }</ProtectedRoute>)
             : element,
-        "errorElement": errorElement
+        "errorElement": <DefaultError />
     }
 }
 
 const router = createBrowserRouter([
-    CreateRoute("*", <>404</>, false),
-    { ...CreateRoute("/", <ExternalHome />, false), index: true },
-    CreateRoute("/user_registry", <UserRegistryScreen />, false),
-    CreateRoute("/login", <LoginScreen />, false),
-    CreateRoute("/home", <InternalHome />),
-    CreateRoute("/account_recovery", <AccountRecoveryScreen />),
-    CreateRoute("/users", <Users />),
-    CreateRoute("/user/:userId", <User />),
-    CreateRoute("/group_registry", (<ProtectedRoute><>Rota não desenvolvida</></ProtectedRoute>)),
-    CreateRoute("/group/:groupId", <>Rota não desenvolvida</>),
-    CreateRoute("/groups", <>Rota não desenvolvida</>),
-    CreateRoute("/board_registry", <>Rota não desenvolvida</>),
-    CreateRoute("/board/:boardId", <>Rota não desenvolvida</>),
-    CreateRoute("/boards", <>Rota não desenvolvida</>),
+    CreateRoute({
+        path: "*",
+        element: (<>404</>),
+        protectedRoute: false
+    }),
+    CreateRoute({
+        path: "/",
+        element: <ExternalHome />,
+        protectedRoute: false
+    }),
+    CreateRoute({
+        path: "/user_registry",
+        element: <UserRegistryScreen />,
+        protectedRoute: false
+    }),
+    CreateRoute({
+        path: "/login",
+        element: <LoginScreen />,
+        protectedRoute: false
+    }),
+    CreateRoute({
+        path: "/home",
+        element: <InternalHome />
+    }),
+    CreateRoute({
+        path: "/account_recovery",
+        element: <AccountRecoveryScreen />
+    }),
+    CreateRoute({
+        path: "/users",
+        element: <Users />
+    }),
+    CreateRoute({
+        path: "/user/:userId",
+        element: <User />
+    }),
+    CreateRoute({
+        path: "/group_registry",
+        element: (<>Rota não desenvolvida</>)
+    }),
+    CreateRoute({
+        path: "/group/:groupId",
+        element: (<>Rota não desenvolvida</>)
+    }),
+    CreateRoute({
+        path: "/groups",
+        element: (<>Rota não desenvolvida</>)
+    }),
+    CreateRoute({
+        path: "/board_registry",
+        element: (<>Rota não desenvolvida</>)
+    }),
+    CreateRoute({
+        path: "/board/:boardId",
+        element: (<>Rota não desenvolvida</>)
+    }),
+    CreateRoute({
+        path: "/boards",
+        element: (<>Rota não desenvolvida</>)
+    }),
 ])
 
 export default router
