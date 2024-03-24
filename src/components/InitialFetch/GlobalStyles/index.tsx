f487babcfe470a2341bb60028586c87708dfab6f
Raisson Souza <raissonrai@gmail.com>
2024-03-24 01:14:21 -0300

    - Refatoração dos estilos globais.

--- src/components/InitialFetch/GlobalStyles/index.tsx
+++ src/components/InitialFetch/GlobalStyles/index.tsx
@@ -8,64 +8,32 @@ export default function GenerateGlobalStyle(systemStyle? : SystemStyle) : JSX.El
 
     const invertedTextColor = InvertedTextColor(systemStyle!.TextColor)
 
-    const globalStyleAll = (
-        <style
-            dangerouslySetInnerHTML={{ __html: `
-                * {
-                    color: ${ systemStyle!.TextColor };
-                };
-            `}}
-        />
-    )
-    const globalStyleInput = (
-        <style
-            dangerouslySetInnerHTML={{ __html: `
-                input {
-                    background-color: ${ invertedTextColor };
-                    border-radius: 5px
-                }
-            `}}
-        />
-    )
-    const globalStyleButton = (
-        <style
-            dangerouslySetInnerHTML={{ __html: `
-                button {
-                    background-color: ${ invertedTextColor }
-                }
-            `}}
-        />
-    )
-    const globalStyleMuiMenu = (
-        <style
-            dangerouslySetInnerHTML={{ __html: `
-                li.MuiButtonBase-root {
-                    color: black !important;
-                }
-            `}}
-        />
-    )
-    const globalStyleMuiDateCalendar = (
-        <style
-            dangerouslySetInnerHTML={{ __html: `
-                .MuiPickersCalendarHeader-label,
-                .MuiPickersArrowSwitcher-root > .MuiIconButton-root > .MuiSvgIcon-root > path,
-                .MuiPickersCalendarHeader-switchViewButton > .MuiSvgIcon-root > path,
-                .MuiYearCalendar-root > .MuiPickersYear-root > button
-                {
-                    color: black !important;
-                }
-            `}}
-        />
-    )
-
     return (
         <>
-            { globalStyleAll }
-            { globalStyleInput }
-            { globalStyleButton }
-            { globalStyleMuiMenu }
-            { globalStyleMuiDateCalendar }
+            <style
+                dangerouslySetInnerHTML={{ __html: `
+                    * {
+                        color: ${ systemStyle!.TextColor };
+                    };
+                    input {
+                        background-color: ${ invertedTextColor };
+                        border-radius: 5px
+                    };
+                    button {
+                        background-color: ${ invertedTextColor }
+                    };
+                    li.MuiButtonBase-root {
+                        color: black !important;
+                    };
+                    .MuiPickersCalendarHeader-label,
+                    .MuiPickersArrowSwitcher-root > .MuiIconButton-root > .MuiSvgIcon-root > path,
+                    .MuiPickersCalendarHeader-switchViewButton > .MuiSvgIcon-root > path,
+                    .MuiYearCalendar-root > .MuiPickersYear-root > button
+                    {
+                        color: black !important;
+                    };
+                `}}
+            />
         </>
     )
 }
